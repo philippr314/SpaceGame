70370e4c295dd58ed2ee39de4f7a0b413c557846
philippr314 <philippr314@gmail.com>
2017-03-08 20:56:36 +0100

    Erased PhysicalData code
    *due to chaos and dysfunction I removed the content of the PhysicalData script
    -> need to take on the problem more scientific

--- Assets/PhysicalData.cs
+++ Assets/PhysicalData.cs
@@ -4,47 +4,4 @@ using UnityEngine;
 
 public class PhysicalData : MonoBehaviour {
 
-    public Star physicalObject;
-
-}
-
-[System.Serializable]
-public class PhysicalObject
-{
-    public long mass;
-    public long radius;
-    public double velocity;
-    public long impulse;
-}
-
-[System.Serializable]
-public class Star : PhysicalObject
-{
-    public double apparentMagnitude;
-    public SpectralClass spectralClass;
-}
-
-[System.Serializable]
-public class SpectralClass
-{
-    public static Dictionary<char, List<string>> types = new Dictionary<char, List<string>>();
-
-    //O, B, A, F, G and so on..
-    public char identifier;
-    public List<string> chemicalElements;
-
-    public SpectralClass(char id)
-    {
-        SetTypes();
-        identifier = id;
-        chemicalElements = types[identifier];
-    }
-
-    private void SetTypes()
-    {
-        types.Add('O', new List<string> {"Ionised Helium"});
-        types.Add('B', new List<string> { "Neutral Helium", "Balmer Series Hydrogen" });
-        //TODO: add rest of the types 
-    }
-
 }
